04ebf5f651027b543a25f52cf4f06b1bc51f1682
Monferd Collin <monferd.j.collin@gmail.com>
2020-01-30 23:02:18 -0500

    added auth page(no passport)

---
+++ auth/index.js
@@ -0,0 +1,39 @@
+const router = require('express').Router()
+const { User } = require('../db/models')
+
+//logging in
+
+router.post('/login', async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        email: req.body.email
+      }
+    })
+    if (!user) {
+      res.status(401).send('The user does not exist')
+    } else if (!user.correctPassword(req.body.password)) {
+      res.status(401).send('Wrong username and password combination')
+    } else {
+      req.login(user, err => err ? next(err) : res.json(user))
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+// logging out
+// waiting for passport to be put in
+
+// router.post('/logout', (req,res) =>{
+//   req.logout()
+//   req.session.destroy()
+//   res.redirect('/')
+// })
+
+
+
+// gettinng current user data (in cookies)
+router.get('/me', (req, res) => {
+  res.json(req.user)
+})
